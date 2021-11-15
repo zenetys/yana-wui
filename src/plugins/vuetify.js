b1d39273d6157800be78faeb0eb1e882c19e35c3
Jules Klu <jklu@zenetys.com>
2021-11-15 14:09:26 +0100

    MINOR: Override vuetify default primary color

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,12 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+          light: {
+            // primary: '#106e84',
+            primary: '#0f6e84'
+          },
+        },
+    },
 });
