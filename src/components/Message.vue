cf391583bc279f32e84df66461348421b70e6870
Jules Klu <jklu@zenetys.com>
2021-11-17 10:03:57 +0100

    FEATURE: message: Component to display brief messages like ajax errors

---
+++ src/components/Message.vue
@@ -0,0 +1,86 @@
+<template>
+    <div>
+        <v-overlay :value="snackbar" :z-index="99" :opacity="0.5" color="#ffffff" @click="closeSnackbarOnButtonClick()">
+            <v-snackbar
+                :timeout="-1"
+                v-model="snackbar"
+                :color="messageColor"
+                :elevation="5"
+                style="white-space: pre-line;"
+                centered
+                ref="messageOverlay"
+                id="messageSnackbar"
+            >
+                <span style="color:rgba(0, 0, 0, 0.87);"> {{this.message.content + (this.message.error ? ('\nError detail: '+this.message.error.message) : '')}} </span>
+                <template v-slot:action="{ attrs }">
+                    <v-btn
+                        icon
+                        v-bind="attrs"
+                        top
+                        @click="closeSnackbarOnButtonClick()"
+                        >
+                        <v-icon size="20" style="color:rgba(0, 0, 0, 0.87);">mdi-close</v-icon>
+                    </v-btn>
+                </template>
+            </v-snackbar>
+        </v-overlay>
+    </div>
+</template>
+
+
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+    name: 'Message',
+    props: {
+    },
+    data() {
+        return {
+            snackbar: false,
+            snackbarTimeOut: 5000,
+        }
+    },
+    computed: {
+        ...mapState({
+            message: 'storeInfoMessage'
+        }),
+        messageColor() {
+            if (this.message.type=='error') {
+                return '#FFADAD';
+            } else if (this.message.type=='info') {
+                return 'primary';
+            } else if (this.message.type=='warning') {
+                return 'orange lighten-4';
+            }
+            return 'primary';
+        }
+    },
+    watch: {
+        message() {
+            if(Object.keys(this.message)!=0){
+                this.snackbar = true;
+            } else {
+                this.snackbar = false;
+            }
+        }
+    },
+    methods: {
+        closeSnackbarOnButtonClick() {
+            this.snackbar = false;
+            this.$store.commit('EDIT_STORE_INFO_MESSAGE', {});
+        },
+        closeSnackbarOnWindowKeyUp(e) {
+            if (e.key == 'Escape')
+                this.closeSnackbarOnButtonClick();
+        }
+    },
+    mounted() {
+        window.addEventListener('keyup', this.closeSnackbarOnWindowKeyUp);
+    },
+    destroyed() {
+        window.removeEventListener('keyup', this.closeSnackbarOnWindowKeyUp);
+    },
+}
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,25 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        storeInfoMessage: {},
+    },
+    mutations: {
+        EDIT_STORE_INFO_MESSAGE(state, value) {
+            console.log('from store: mutation EDIT_STORE_INFO_MESSAGE', value);
+            state.storeInfoMessage = value;
+        }
+    },
+    actions: {
+        updateStoreInfoMessage({ commit }, value) {
+            commit('EDIT_STORE_INFO_MESSAGE', value);
+        }
+    },
+    getters: {
+        storeInfoMessage: state => {
+            return state.storeInfoMessage;
+        }
+    },
+    modules: {
+    }
 })
