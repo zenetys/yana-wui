0bd0bf9ab8cf83ea39fd802e1adc4a96ef4ee12a
Julien Thomas <jthomas@zenetys.com>
2022-06-20 08:32:26 +0200

    MAJOR: message: New impementation of the Message component
    Triggering messages is now event based. To render a message on the
    UI, emit a message event through the event bus plugin. The following
    events are also available as shortcuts: info, warning, error.
    
    Example:
    
    this.$ev.$emit('message', { type: 'info', title: 'Message title',
        text: 'Some text for the message', detail: 'Some more',
         duration: 2000 });
    
    Arguments expected for each supported events:
    - message: object, spec below
    - info: title[, text[, detail]]
    - warning: title[, text[, detail]]
    - error: [error, ]title[, text[, detail]]
    
    Message object properties:
    - type: info, warning, error (default info)
    - title: should be provided, except if error is present
    - text: should be provided, except if error is present
    - detail: optional extra text
    - duration: in ms, override default time the message is visible
    - error: optional Error object
    
    Passing an Error object through to error property will automatically
    set values for properties type, title, text and potentially detail,
    if not explicitly given.

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,90 +1,210 @@
 <template>
-    <div>
-        <v-overlay :value="snackbarIsDisplayed" :z-index="99" :opacity="0.5" color="#ffffff" @click="closeSnackbar()">
-            <v-snackbar
-                :timeout="-1"
-                v-model="snackbarIsDisplayed"
-                :color="messageColor"
-                :elevation="5"
-                style="white-space: pre-line"
-                centered
-                ref="messageOverlay"
-                id="messageSnackbar">
-                <span class="message"> {{ messageContent }} </span>
-                <template v-slot:action="{ attrs }">
-                    <v-btn icon v-bind="attrs" top @click="closeSnackbar()">
-                        <v-icon class="message">mdi-close</v-icon>
-                    </v-btn>
-                </template>
-            </v-snackbar>
-        </v-overlay>
+    <div class="messages">
+        <div v-for="(entry) in messages" :key="entry.id" class="entry">
+            <transition
+                :appear="true"
+                :css="false"
+                @before-enter="onBeforeEnter"
+                @enter="(el, done) => { onEnter(el, done, entry) }"
+                @leave="(el, done) => { onLeave(el, done, entry) }"
+                @after-leave="(el) => { onAfterLeave(el, entry) }"
+                @leave-cancelled="(el) => { onAfterLeave(el, entry) }"
+            >
+                <v-alert
+                    @click="deleteMessage(entry)"
+                    border="left"
+                    elevation="10"
+                    colored-border
+                    :type="entry.type"
+                    v-show="entry.show"
+                >
+                    <div class="text-subtitle-2">{{ entry.title }}</div>
+                    <div class="body-2">{{ entry.text }}</div>
+                    <div class="text-caption">{{ entry.detail }}</div>
+                </v-alert>
+            </transition>
+        </div>
     </div>
 </template>
 
-<style lang="scss">
-.message {
-    color: rgba(0, 0, 0, 0.87) !important;
+<style scoped>
+.messages {
+    position: fixed;
+    top: 1rem;
+    bottom: 1rem;
+    right: 1rem;
+    min-width: 20rem;
+    max-width: 30rem;
+    display: flex;
+    word-break: break-word;
+    /*flex-direction: column;
+    justify-content: flex-end;*/
+    flex-direction: column-reverse;
+    justify-content: flex-end;
+
+    /* alerts are always up front but the whole message panel
+     * does not prevent :hover on elements below it... */
+    z-index: 99;
+    visibility: hidden;
+}
+.entry {
+    /* ...cancel visibility: hidden of the parent */
+    visibility: visible;
+}
+
+/* allow :hover to pass through elements visible between alerts */
+.v-alert { margin-bottom: 0; }
+.entry + .entry { margin-bottom: 16px; }
+
+.text-caption {
+    font-size: 0.68rem !important;
+    line-height: 0.9rem !important;
 }
 </style>
 
 <script>
+import axios from 'axios';
+axios.buildFullPath = require('axios/lib/core/buildFullPath');
+axios.buildURL = require('axios/lib/helpers/buildURL');
+
+function axiosError2URL(e) {
+    const method = e.config.method.toUpperCase();
+    return (method === 'POST' || method === 'PUT' || method === 'PATCH')
+        ? (method + ' ' + axios.buildFullPath(e.config.baseURL, e.config.url))
+        : (method + ' ' + axios.buildFullPath(e.config.baseURL, axios.buildURL(e.config.url, e.config.params)));
+}
+
+function loopWhile(actionCb, whileCb, delay) {
+    setTimeout(() => {
+        if (!whileCb())
+            return;
+        actionCb();
+        loopWhile(actionCb, whileCb, delay);
+    }, delay);
+}
+
 export default {
     name: 'Message',
-    props: {},
-    data() {
-        return {
-            snackbarIsDisplayed: false,
-            snackbarTimeOut: 5000,
-        };
+    props: {
+        displayDuration: {
+            type: Number,
+            required: false,
+            default: 10000,
+        },
+        fadeInStepDuration: {
+            type: Number,
+            required: false,
+            default: 30,
+        },
+        fadeOutStepDuration: {
+            type: Number,
+            required: false,
+            default: 40,
+        },
     },
+    data: () => ({
+        messages: [],
+    }),
     computed: {
-        message: {
-            get() {
-                return this.$store.getInfoMessage();
-            },
-            set(newMessage) {
-                this.$store.setInfoMessage(newMessage);
-            },
-        },
-        messageColor() {
-            switch (this.message.type) {
-                case 'error':
-                    return '#FFADAD';
-                case 'info':
-                    return 'primary';
-                case 'warning':
-                    return 'orange lighten-4';
-                default:
-                    return 'primary';
-            }
-        },
-        messageContent() {
-            return this.message.content + (this.message.error ? '\nError detail: ' + this.message.error.message : '');
-        },
-    },
-    watch: {
-        message() {
-            if (Object.keys(this.message).length !== 0) {
-                this.snackbarIsDisplayed = true;
-            } else {
-                this.snackbarIsDisplayed = false;
-            }
-        },
     },
     methods: {
-        closeSnackbar() {
-            this.snackbarIsDisplayed = false;
-            this.message = {};
+        /* v-show fade transitions */
+        onBeforeEnter(el) {
+            el.style.opacity = 0;
         },
-        closeSnackbarOnWindowKeyUp(e) {
-            if (e.key === 'Escape') this.closeSnackbar();
+        onEnter(el, done, entry) {
+            let opacity = Number(el.style.opacity);
+            loopWhile(
+                () => { opacity += 0.1; el.style.opacity = opacity; },
+                () => { if (opacity < 1 && entry.show) return true; done(); return false; },
+                this.fadeInStepDuration
+            );
+        },
+        onLeave(el, done, entry) {
+            let opacity = Number(el.style.opacity);
+            loopWhile(
+                () => { opacity -= 0.1; el.style.opacity = opacity; },
+                () => { if (opacity >= 0 && !entry.show) return true; done(); return false; },
+                this.fadeOutStepDuration
+            );
+        },
+        onAfterLeave(el, entry) {
+            this.deleteMessage(entry, true);
+        },
+
+        deleteMessage(entry, flush = false) {
+            clearTimeout(entry.timer);
+            entry.show = false; /* triggers leave transition */
+            if (flush) {
+                this.messages.splice(entry.index, 1);
+                for (let i = entry.index; i < this.messages.length; i++)
+                    this.messages[i].index--;
+            }
+        },
+        onWindowKeyUp(e) {
+            if (e.key === 'Escape') {
+                for (let i = this.messages.length - 1; i >= 0; i--)
+                    this.deleteMessage(this.messages[i]);
+            }
+        },
+
+        onMessage(entry) {
+            if (entry.error) {
+                entry.type ||= 'error';
+                entry.title ||= 'An error has occurred';
+                entry.text = entry.error.toString();
+                if (!entry.detail && axios.isAxiosError(entry.error))
+                    entry.detail = axiosError2URL(entry.error);
+            }
+            entry.type ||= 'info';
+            const timerCb = () => { this.deleteMessage(entry); };
+            entry.id = Date.now() + Math.random();
+            entry.show = true;
+
+            if (typeof entry.duration == 'number')
+                entry.timer = setTimeout(timerCb, entry.duration);
+            else if (entry.duration === undefined)
+                entry.timer = setTimeout(timerCb, this.displayDuration);
+            else
+                entry.timer = null;
+
+            entry.index = this.messages.push(entry) - 1;
+
+            console.log(`Message: [${entry.type.toUpperCase()}] ${entry.title}`,
+                (entry.text ? ' | ' + entry.text : ''),
+                (entry.detail ? ' | ' + entry.detail : ''));
+        },
+        onErrorMessage(...args) {
+            let error = undefined;
+            if (args[0] instanceof Error) {
+                error = args[0];
+                args.shift();
+            }
+            this.onMessage({ type: 'error', title: args[0], text: args[1],
+                detail: args[2], error });
+        },
+        onWarningMessage(...args) {
+            this.onMessage({ type: 'warning', title: args[0], text: args[1],
+                detail: args[2] });
+        },
+        onInfoMessage(...args) {
+            this.onMessage({ type: 'info', title: args[0], text: args[1],
+                detail: args[2] });
         },
     },
     mounted() {
-        window.addEventListener('keyup', this.closeSnackbarOnWindowKeyUp);
+        window.addEventListener('keyup', this.onWindowKeyUp);
+        this.$ev.$on('message', this.onMessage);
+        this.$ev.$on('error', this.onErrorMessage);
+        this.$ev.$on('warning', this.onWarningMessage);
+        this.$ev.$on('info', this.onInfoMessage);
     },
     destroyed() {
-        window.removeEventListener('keyup', this.closeSnackbarOnWindowKeyUp);
+        window.removeEventListener('keyup', this.onWindowKeyUp);
+        this.$ev.$off('message', this.onMessage);
+        this.$ev.$off('error', this.onErrorMessage);
+        this.$ev.$off('warning', this.onWarningMessage);
+        this.$ev.$off('info', this.onInfoMessage);
     },
 };
 </script>
