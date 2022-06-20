512713a5e00b3544232a3277e3d9db37b43a9d9a
Julien Thomas <jthomas@zenetys.com>
2022-06-20 19:06:55 +0200

    BUG/MINOR: init: Rework main init so we can notify early errors
    The patch reworks the initialization of the application in order
    to make it possible to notify the user of early errors. Such errors
    are related to axios requests that take place before Vue gets
    instanciated. These errors now get buffered, then rendered using
    the new Message component once the Vue app is mounted.

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import router from '@/router';
 import vuetify from '@/plugins/vuetify';
 import Api from '@/plugins/api';
 import * as Utils from '@/plugins/utils';
-import Entities from '@/plugins/entities';
 import Store from '@/plugins/store';
 import Ev from '@/plugins/evbus';
 
@@ -18,15 +17,34 @@ Vue.prototype.$store = Store;
 Vue.prototype.$ev = Ev;
 
 async function init() {
-    /* Initialise the API plugin */
-    await Api.init();
-    /* Fetch all Entities */
-    await Entities.fetchStoreEntities();
+    const earlyErrors = [];
 
+    /* Initialise the API plugin */
+    try { await Api.init() }
+    catch (e) {
+        earlyErrors.push({ title: 'Failed to initialize API',
+            error: e, duration: null });
+    }
+
+    /* Fetch entities */
+    try {
+        const entities = await Api.axiosData('/entities', false);
+        Store.setEntities(entities);
+    }
+    catch (e) {
+        earlyErrors.push({ title: 'Failed to retrieve entities',
+            error: e, duration: null });
+    }
+
+    /* Start Vue */
     new Vue({
         router,
         vuetify,
         render: (h) => h(App),
+        mounted: () => {
+            /* Render early errors */
+            earlyErrors.forEach((ee) => Ev.$emit('message', ee));
+        }
     }).$mount('#app');
 }
 
