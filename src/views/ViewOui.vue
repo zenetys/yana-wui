6dcad57197ee3c957e2e0bb09a2271996c6d5ac0
Julien Thomas <jthomas@zenetys.com>
2022-08-25 15:51:31 +0200

    MINOR: oui: v-col should be a direct child of v-row
    As stated in Vuetify documentation, "v-col is a content holder that
    must be a direct child of v-row".
    
    Source: https://vuetifyjs.com/en/components/grids/#v-col

--- src/views/ViewOui.vue
+++ src/views/ViewOui.vue
@@ -1,35 +1,37 @@
 <template>
-    <v-col cols="12" sm="12" md="6" lg="6" xl="6" class="pt-0 pl-0">
-        <h2 class="pb-6">OUI lookup tool</h2>
-        <div class="pb-2">Enter MAC addresses or prefixes to lookup vendors.<br /></div>
-        <div class="pb-1 text-caption font-weight-bold">Examples:</div>
-        <div class="pb-8 text-body-2 font-weight-light">08:00:20<br />98:FA-9B-63-0C-C4<br />00d9.d110.21f9<br /></div>
-        <h4>OUI search:</h4>
-        <v-textarea
-            v-model="ouiSearch"
-            placeholder="00:00:00:00:00:00
+    <v-row no-gutters>
+        <v-col cols="12" sm="12" md="6" lg="6" xl="6" class="pt-0 pl-0">
+            <h2 class="pb-6">OUI lookup tool</h2>
+            <div class="pb-2">Enter MAC addresses or prefixes to lookup vendors.<br /></div>
+            <div class="pb-1 text-caption font-weight-bold">Examples:</div>
+            <div class="pb-8 text-body-2 font-weight-light">08:00:20<br />98:FA-9B-63-0C-C4<br />00d9.d110.21f9<br /></div>
+            <h4>OUI search:</h4>
+            <v-textarea
+                v-model="ouiSearch"
+                placeholder="00:00:00:00:00:00
 11:11:11:11:11:11
 ..."
-            :spellcheck="false"
-            :hide-details="true"
-            dense
-            class="pb-4"
-        />
-        <v-btn
-            @click="handleOuiLookupClick"
-            color="#17B8CE"
-            class="white--text"
-        >Find</v-btn>
-        <div class="pt-8">
-            <table v-if="typeof ouiMessage === 'object'">
-                <tr v-for="(subMessage, subMessageKey) in ouiMessage" :key="subMessageKey">
-                    <td class="pr-2">{{ subMessageKey }}</td>
-                    <td>{{ subMessage }}</td>
-                </tr>
-            </table>
-            <p v-else>{{ ouiMessage }}</p>
-        </div>
-    </v-col>
+                :spellcheck="false"
+                :hide-details="true"
+                dense
+                class="pb-4"
+            />
+            <v-btn
+                @click="handleOuiLookupClick"
+                color="#17B8CE"
+                class="white--text"
+            >Find</v-btn>
+            <div class="pt-8">
+                <table v-if="typeof ouiMessage === 'object'">
+                    <tr v-for="(subMessage, subMessageKey) in ouiMessage" :key="subMessageKey">
+                        <td class="pr-2">{{ subMessageKey }}</td>
+                        <td>{{ subMessage }}</td>
+                    </tr>
+                </table>
+                <p v-else>{{ ouiMessage }}</p>
+            </div>
+        </v-col>
+    </v-row>
 </template>
 
 <style scoped lang="scss">
