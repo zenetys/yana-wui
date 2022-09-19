2d504a77978e04fd4a594ac7ebc94d808a41c851
Julien Thomas <jthomas@zenetys.com>
2022-09-19 15:35:01 +0200

    MINOR: api: Remove old get() axios wrapper not used anymore

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -1,34 +1,7 @@
 import axios from 'axios';
 import Ev from '@/plugins/evbus';
 
-/**
- * Handle errors from the API and display them to the user.
- * @deprecated Will be removed along with get() from the exported object
- *
- * @param {object} error - The error object from the API
- * @param {string} errorContext - The context to be displayed in the error message
- */
-function handleError(error, errorContext) {
-    const defaultErrorMessage = 'Something went wrong while communicating with the server.';
-    Ev.$emit('error', error, errorContext || defaultErrorMessage);
-}
-
 export default {
-    /**
-     * Make a GET request to the API
-     * @deprecated Use axiosData instead
-     *
-     * @param {string} url - the URL to call
-     * @param {string} errorContext - The context to be displayed in case of error
-     * @param {AxiosRequestConfig} config - The config to be passed to axios
-     * @returns {*} - The response from the API
-     */
-    async get(url, errorContext, config) {
-        return await axios
-            .get(url, config)
-            .then((response) => response.data)
-            .catch((error) => handleError(error, errorContext));
-    },
 
     axiosData(urlOrConfig, errorMessage) {
         const config = typeof urlOrConfig === 'string' ? { url: urlOrConfig } : urlOrConfig;
