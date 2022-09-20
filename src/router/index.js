a85e7dcd01eb1cc97c75b861930be370f65dd3a9
Julien Thomas <jthomas@zenetys.com>
2022-09-20 10:15:50 +0200

    FEATURE: router: Generate app menu from routes definition
    We make use of the meta object available in routes definition
    to attach information used to generate the main app menu.
    
    The result is accessible via a getMenu() method attached to
    Vue router, eg. this.$router.getMenu() in components.
    
    This will be used by ViewMain in a next commit so we can get
    the menu definition out of it.

--- src/router/index.js
+++ src/router/index.js
@@ -35,6 +35,14 @@ const routes = [
                 path: '/main/inventory',
                 name: 'ViewInventory',
                 component: ViewInventory,
+                meta: {
+                    menu: [
+                        {
+                            label: 'Inventory',
+                            icon: 'mdi-grid',
+                        },
+                    ],
+                },
                 children: [
                     {
                         path: '/main/inventory/host/:id',
@@ -52,11 +60,38 @@ const routes = [
                 path: '/main/vlan-matrix',
                 name: 'ViewVlanMatrix',
                 component: ViewVlanMatrix,
+                meta: {
+                    menu: [
+                        {
+                            label: 'VLAN matrix',
+                            icon: 'mdi-table',
+                        },
+                    ],
+                },
+            },
+            {
+                path: '/main/l2-diagram',
+                meta: {
+                    menu: [
+                        {
+                            label: 'L2 diagram',
+                            icon: 'mdi-graph',
+                        },
+                    ],
+                },
             },
             {
                 path: '/main/oui',
                 name: 'ViewOui',
                 component: ViewOui,
+                meta: {
+                    menu: [
+                        {
+                            label: 'OUI lookup',
+                            icon: 'mdi-help-network-outline',
+                        },
+                    ],
+                },
             },
         ],
     },
@@ -68,6 +103,31 @@ const routes = [
         },
     },
 ];
+
+VueRouter.prototype.getMenu = function () {
+    var menu = [];
+    function walk(routes) {
+        for (let r of routes) {
+            if (r.meta && r.meta.menu) {
+                for (let m of r.meta.menu) {
+                    const entry = { label: m.label, icon: m.icon };
+                    if (r.name) /* require a named route */ {
+                        entry.route = { name: r.name };
+                        if (m.query)
+                            entry.route.query = m.query;
+                        if (m.params)
+                            entry.route.params = m.params;
+                    }
+                    menu.push(entry);
+                }
+            }
+            if (r.children)
+                walk(r.children)
+        }
+    }
+    walk(this.getRoutes());
+    return menu;
+}
 
 const router = new VueRouter({
     routes,
